d3820ecf1140e00e23252572280b51c605d04f97
Robson Marques <robsonmarques@gmail.com>
2017-01-30 05:28:24 -0200

    Ajusta a exibição do lead no ActiveAdmin
    Para os casos onde o nome do lead não está preenchido, o ideal é
    usar sempre o email como referência para exibição.

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -4,6 +4,8 @@ class Lead < ApplicationRecord
   has_many :subscriptions, dependent: :destroy
   has_many :lists, -> { merge(Subscription.active) }, through: :subscriptions
 
+  alias_attribute :display_name, :to_s
+
   def to_s
     email
   end
